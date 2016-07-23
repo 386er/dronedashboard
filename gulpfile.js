1a7f73b2401375225cd942354419cbebafb95b5a
386er <t.burri@gmx.de>
2016-07-23 21:09:46 +0200

    Introduce browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 
 
-/*gulp.task('default', function() {
-	gulp.watch('./src/styles/sass/**.scss', ['build']);
-});
-*/
 
 gulp.task('styles', function() {
-	gulp.src([
+	console.log('Run Styles');
+	return gulp.src([
 		'./src/style/sass/style.scss',
 		'./src/style/sass/jquery.gridster.css'])
 		.pipe(sass().on('error', sass.logError))
@@ -29,6 +28,45 @@ gulp.task('move', function() {
 });
 
 
+
 gulp.task('build', ['move', 'styles']);
+
+
+gulp.task('watch', function() {
+	return gulp.watch('src/**/*.scss', function(event) {
+		gulp.start('build');
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	});	
+});
+
+
+
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:3000",
+        files: ["build/**/*.*"],
+        browser: "google chrome",
+        port: 5000,
+	});
+});
+
+gulp.task('nodemon', function (cb) {
+	var started = false;
+	
+	return nodemon({
+		script: 'index.js'
+	}).on('start', function () {
+		// to avoid nodemon being started multiple times
+		// thanks @matthisk
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
 
 
